8a219d829552891ebd40c749d13fff6347e87b06
Fem Yale <femi@livicode.com>
2016-07-06 09:30:39 +0100

    Modules was touched upon here

---
+++ public/app3.js
@@ -0,0 +1,8 @@
+//modules
+//breaking bunch of codes into different pieces = modules
+
+//to add a module to my code, use this
+//no need to include the .js extension
+var movies = require('./movies');
+
+movies.avatar();
---
+++ public/movies.js
@@ -0,0 +1,10 @@
+function printAvatar(){
+	console.log("This is Avatar Movie");
+}
+
+function printchappie(){
+	console.log("Chappie: R");
+}
+
+//I am saying that this is the only part of this file that must be exported
+module.exports.avatar = printAvatar;
